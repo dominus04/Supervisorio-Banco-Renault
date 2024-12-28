469c2bf40ea504764a355064a96344955537a9ed
dominus04 <igorctmdt@hotmail.com>
2024-12-28 09:49:07 -0300

    Initting the creation of User Manager Window

---
+++ Supervisório Banco Renault/Views/UsersManager.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Supervisório_Banco_Renault.Views
+{
+    /// <summary>
+    /// Interação lógica para UsersManager.xam
+    /// </summary>
+    public partial class UsersManager : UserControl
+    {
+        public UsersManager()
+        {
+            InitializeComponent();
+        }
+    }
+}
